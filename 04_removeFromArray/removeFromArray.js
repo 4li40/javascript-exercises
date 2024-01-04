a670761390200c63c402d4fac39cefc0b134737d
Ali <alii.osman2003@gmail.com>
2024-01-04 17:04:21 +0200

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...element) {
+    
+    return arr.filter(item => !element.includes(item));
+    
 };
 
 // Do not edit below this line
