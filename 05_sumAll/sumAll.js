b46259992e6a12e8776b81c04f67875243e887f9
Ali <alii.osman2003@gmail.com>
2024-01-04 17:23:16 +0200

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(nb1,nb2) {
 
+    if(!Number.isInteger(nb1) || !Number.isInteger(nb2)) return "ERROR";
+    if(nb1 < 0 || nb2 < 0) return "ERROR";
+    if(nb1 > nb2){
+        const temp = nb1;
+        nb1 = nb2;
+        nb2 = temp;
+    }
+
+    let sum = 0;
+    for(let i = nb1; i <= nb2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
