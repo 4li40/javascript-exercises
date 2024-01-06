3b1d4e19da900d6e973f8c354c200df79f8f924f
Ali <alii.osman2003@gmail.com>
2024-01-06 09:04:14 +0200

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowercase = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reverse = lowercase.split("").reverse().join("");
+    return reverse === lowercase;
 };
 
 // Do not edit below this line
